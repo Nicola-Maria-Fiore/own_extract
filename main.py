e337bd28c87c5d8a33e206ca70cd5cbc7a423d6e
ferru97 <vitof970@gmail.com>
2021-01-23 12:53:41 +0100

    file upload

---
+++ main.py
@@ -0,0 +1,51 @@
+import xlsxwriter
+import keyboard
+import time
+import os
+
+out = "results/"
+company = "company.csv"
+year = "year.csv"
+
+sheets = 5
+excel_path = "C:\Program Files\Microsoft Office\Office16\EXCEL.exe"
+formula = '=@RDP.Data("INSERT","TR.HoldingsDate;TR.EarliestHoldingsDate;TR.PctOfSharesOutHeld;TR.SharesHeld;TR.SharesHeldValue;TR.InvestorFullName;TR.FilingType;TR.InvParentType;TR.OwnTrnverRating;TR.OwnTrnverRatingCode;TR.OwnTurnover;TR.NbrOfInstrHeldByI"&"nv;TR.NbrOfInstrBoughtByInv;TR.NbrOfInstrSoldByInv;TR.PctPortfolio;TR.InvestorType;TR.InvInvestmentStyleCode;TR.InvInvmtOrientation;TR.InvestorRegion;TR.InvAddrCountry","SDate=YEAR-12-31 CH=Fd RH=IN",A1)'
+
+def createXslc(comapny_id, year_list):
+    workbook = xlsxwriter.Workbook(out+comapny_id+'.xlsx')
+
+    for y in year_list:
+        worksheet = workbook.add_worksheet()
+        content = formula.replace("INSERT",comapny_id).replace("YEAR",y)
+        print(content)
+        worksheet.write('A1', "content")
+    
+    workbook.close()
+
+def openFiles(companies):
+    for c in companies:
+        f_path = out + c + ".xlsx"
+        f_path = os.path.abspath(f_path)
+        os.system('start "{}" "{}"'.format(excel_path,f_path))
+        time.sleep(2)
+        keyboard.send('ctrl+s')
+        time.sleep(2)
+        keyboard.send('alt+F4')
+
+def readFile(fname):
+    result = []
+    with open(fname, 'r', encoding='utf-8') as file_in:
+        for line in file_in:
+            result.append(line.strip().replace('\n',""))
+    return result
+
+if __name__ == "__main__":
+    companies = readFile(company)
+    years = readFile(year)
+
+    for c in companies:
+        createXslc(c,years)
+    
+    #openFiles(companies)
+   
+    print("Done!")
